443e2fed502d49e127d69032c2ff8963cf8f8cbe
bithir <bithir@gmail.com>
2021-02-20 21:03:13 +0000

    Fixed name regex

--- script/npcimporter.js
+++ script/npcimporter.js
@@ -95,7 +95,7 @@ async function extractAllData(npcData)
 
     let extractData = function(inputData, inputPattern) {
         let tmp = inputData.match(inputPattern);
-        if( tmp != null && tmp.length == 2) {
+        if( tmp != null && tmp.length >= 2) {
             // successful match
             return tmp[1];
         }
@@ -109,7 +109,7 @@ async function extractAllData(npcData)
     // npcData = "Cryptwalker Race Spirit Resistance Strong Traits Gravely cold (III), Manifestation (III), Spirit form (III) Accurate 5 (+5), Cunning 10 (0), Discreet 7 (+3), Persuasive 10 (0), Quick 11 (−1), Resolute 13 (−3), Strong 15 (−5), Vigilant 9 (+1) Abilities Iron Fist (master), Twin Attack (master) Weapons 2 swords 7/6 (balanced), two Strong attacks against the same target Armor None, only mystical powers and magical weapons are harmful, only with half damage Defense −3 (two weapons) Toughness 15 Pain Threshold — Equipment Two wraith blades (quality: Balanced) Shadow Like a clear night sky, with faint light that does nothing but make the dark seem blacker (thoroughly corrupt) Tactics: The cryptwalker assumes that the ene- my will have a hard time damaging it, until proven otherwise. Either way it uses its gravely cold power to paralyze enemies, then finishes them off with the swords.";
     let expectedData = npcData.replaceAll("- ","");
 
-    let namePattern = /^(.+?) [Race|Manner]+/;
+    let namePattern = /^(.+?) (Race|Manner)/;
     let newValues = {
         name: extractData(expectedData,namePattern),
         type: "monster",
