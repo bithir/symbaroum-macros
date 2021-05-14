eea67264896f08af18f1c2f871cc58bef952610b
bithir <bithir@gmail.com>
2021-05-14 21:09:48 +0100

    Tokenbar/name changer

---
+++ script/TokenBarChange.js
@@ -0,0 +1,20 @@
+/* This will set every token in scene to always display their
+ * token bars and nameplate, and sets the first bar to represent 
+ * HP and removes the second token bar.
+*/
+
+// NONE - no information is displayed 
+// CONTROL - displayed when the token is controlled 
+// OWNER_HOVER - displayed when hovered by a GM or a user who owns the actor 
+// HOVER - displayed when hovered by any user 
+// OWNER - always displayed for a GM or for a user who owns the actor 
+// ALWAYS - always displayed for everyone
+let tokens =  canvas.tokens.controlled.map( token => {
+   return { _id: token._id,
+         "bar2.attribute": "",
+         "displayName": CONST.TOKEN_DISPLAY_MODES.ALWAYS,
+         "displayBars": CONST.TOKEN_DISPLAY_MODES.ALWAYS
+   }
+});
+if( tokens && tokens.length > 0)
+   canvas.tokens.updateMany(tokens);
